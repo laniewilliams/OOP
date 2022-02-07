51033ebb546770d5ae37b00f552ff47f5505b079
Lanie Williams <lanie_williams@baylor.edu>
2022-02-07 14:19:46 -0600

    in class

---
+++ Fly.py
@@ -0,0 +1,16 @@
+import InsectClass as i
+
+
+def main():
+
+    my_insect = i.Insect()
+
+    
+
+    my_insect.flight()
+
+    #my_insect.get_flight()
+
+    print('The length of flight:',my_insect.get_flight())
+
+main()
