70f01d8e8c8a8928e85035e33e1c9918250f685e
Lanie Williams <lanie_williams@baylor.edu>
2022-02-09 13:39:46 -0600

    OOP

--- Fly.py
+++ Fly.py
@@ -3,13 +3,13 @@ import InsectClass as i
 
 def main():
 
-    my_insect = i.Insect()
+    my_insect = i.Insect(2,4)
 
    
     my_insect.flight_length()
 
    
 
-    print('Length of flight:',my_insect.get_flight())
+    print('Length of flight:',my_insect.get_flight(),'miles')
 
 main()
