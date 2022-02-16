e9f6a7fb343ccc6c207d9ceb77213c509f7ed706
Lanie Williams <lanie_williams@baylor.edu>
2022-02-16 13:00:32 -0600

    Retail work

--- RetailItemProgram.py
+++ RetailItemProgram.py
@@ -11,7 +11,7 @@ item1 = ri.RetailItem(descr[0],units[0],p[0])
 item2 = ri.RetailItem(descr[1],units[1],p[1])
 item3 = ri.RetailItem(descr[2],units[2],p[2])
 
-
+'''
 item1.set_description(descr[0])
 item2.set_description(descr[1])
 item3.set_description(descr[2])
@@ -24,10 +24,10 @@ item1.set_price(p[0])
 item2.set_price(p[1])
 item3.set_price(p[2])
 
-def show_item_attributes(item):
-    print("List of Attributes: ", item.get_description(),item.get_units(),item.get_price())
+'''
+
 
 print("Item 1: ","Description:",item1.get_description(),", Units in Inventory: ",item1.get_units(), ", Price: $",item1.get_price())
-print("Item 2: ",item2.get_description(), item2.get_units(), item2.get_price())
-print("Item 3: ",item3.get_description(), item3.get_units(), item3.get_price())
+print("Item 2: ","Description:",item2.get_description(),", Units in Inventory: ",item2.get_units(), ", Price: $",item2.get_price())
+print("Item 3: ","Description:",item3.get_description(),", Units in Inventory: ",item3.get_units(), ", Price: $",item3.get_price())
 
